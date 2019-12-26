7ff8ea9d655784f6b8d32e282535988120bae8a0
Sara Gomes <sarah.gomes80@hotmail.com>
2019-12-26 15:25:30 -0300

    Otimização do código com Template Engine

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+const recipes = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
 server.get("/", function(req, res) {
-    return res.send("HI, olaaaa, laaaa")
+    let recipesFilter = []
+    
+    for (let count = 0; count < 6; count++){
+        recipesFilter.push(recipes[count])
+        
+    }
+    return res.render("home", {items: recipesFilter})
+    
+})
+
+server.get("/recipes", function(req, res) {
+    return res.render("recipes", {items: recipes})
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+    
 })
 
 server.listen(5000, function() {
